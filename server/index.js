df7c502a0c7961e3ea3dab222f635c304abbda3f
laggingreflex <laggingreflex@gmail.com>
2017-05-10 01:08:16 +0530

    remove web interface

--- server/index.js
+++ server/index.js
@@ -1,61 +1,19 @@
-/*
-  todo run on https - we're exchanging secret (which though itself encrypted) can be sniffed by man in middle attack
-*/
-
-const proximify = require('proximify');
+const fs = require('fs-extra');
+const Path = require('path');
 const IO = require('socket.io');
 const ss = require('socket.io-stream');
-const http = require('http');
-const Koa = require('koa');
-const esModules = require('koa-es-modules-rollup');
-const bodyparser = require('koa-bodyparser');
-const route = require('koa-route');
+const proximify = require('proximify');
 const secret = require('../utils/secret');
-const fs = require('fs-extra');
-const html = require('./client/html');
 const debug = require('debug')('socket-file-sync');
 
 module.exports = server;
 
 async function server(config) {
-  const app = proximify(new Koa());
-  const server = proximify(http.createServer(app.callback()));
-  const io = proximify(IO(server));
+  const io = IO(config.port);
 
-  app.keys = [config.secret];
-
-  app.use(bodyparser());
-
-  // handle errors
-  app.use((ctx, next) => next().catch(err => {
-    console.error(ctx.url, err);
-    ctx.body = err.message;
-    ctx.status = 500;
-  }));
-
-  app.use(esModules({
-    root: __dirname + '/client',
-  }));
-
-  app.use(route.post('/auth', async ctx => {
-    if (!ctx.request.body || !ctx.request.body.secret) {
-      throw new Error('Need a body');
-    }
-    if (await secret.verify(ctx.request.body.secret)) {
-      ctx.body = { success: true, token: config.secret };
-    } else {
-      throw new Error('Wrong secret');
-    }
-  }));
-
-  // render main app
-  app.use(async ctx => ctx.body = html);
-
-  // start socket.io
   io.on('connection', socket => onConnection(proximify(socket), config));
 
-  await server.listenAsync(config.port);
-  console.log('Listening on port', config.port);
+  console.log('Listening for connections');
 }
 
 async function onConnection(socket, config) {
@@ -87,9 +45,9 @@ async function onConnection(socket, config) {
     await fs.ensureFile(path);
     const stream = proximify(ss.createStream());
     ss(socket).emit('file', stream);
-    stream.pipe(fs.createWriteStream(path));
-    // stream.pipe(process.stdout);
+    // stream.pipe(fs.createWriteStream(path));
+    stream.pipe(process.stdout);
     await stream.onceAsync('end');
-    console.log('Received', file);
+    console.log('Received', relative);
   });
 }
